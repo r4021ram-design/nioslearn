43133d069e410d67d8eb294faf3fbc2703ce0ac9
ramram <r4021ram@gmail.com>
2026-02-18 09:43:34 +0530

    fix: include books in serverless bundle via outputFileTracingIncludes

--- next.config.js
+++ next.config.js
@@ -13,7 +13,11 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // config options here
+  /* config options here */
+  serverExternalPackages: ['pdf-parse'],
+  outputFileTracingIncludes: {
+    '/**': ['./public/books/**/*'],
+  },
 };
 
 module.exports = withPWA(nextConfig);
